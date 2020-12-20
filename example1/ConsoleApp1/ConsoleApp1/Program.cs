caa653b29ca9f7eee5f56becf9c2804ab1afd9dd
zainabAlhaddad <76396982+zainabAlhaddad@users.noreply.github.com>
2020-12-20 19:50:15 +0300

    classwork1

---
+++ example1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String characterName ="Ahmad" , SuperPower1= "Steel Sheild" , SuperPower2="Night Vision";
+            int age = 15;
+            float speed= 1.65f;
+            Console.WriteLine("My Name is "+characterName+" and my powers are  "+SuperPower1 +" and "+SuperPower2+" I am "+age+" years old and my speed is "+speed);
+        }
+    }
+}
