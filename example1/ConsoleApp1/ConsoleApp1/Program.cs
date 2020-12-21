a1670e96646143165a5128a0783b8fccf84349e8
zainabAlhaddad <76396982+zainabAlhaddad@users.noreply.github.com>
2020-12-21 17:43:53 +0300

    Update!

--- example1/ConsoleApp1/ConsoleApp1/Program.cs
+++ example1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,8 +8,22 @@ namespace ConsoleApp1
         {
             String characterName ="Ahmad" , SuperPower1= "Steel Sheild" , SuperPower2="Night Vision";
             int age = 15;
+
+            String HeroName = "Zoey" ,heroSuperPower1 = "Disappear", heroSuperPower2= " night vision" ;
+            int heroHigh = 171, heroAge = 27 ;
+
+
             float speed= 1.65f;
             Console.WriteLine("My Name is "+characterName+" and my powers are  "+SuperPower1 +" and "+SuperPower2+" I am "+age+" years old and my speed is "+speed);
+
+            int diff = heroAge - age;
+            Console.WriteLine("The age difference between two hero are " + heroAge + " - " + age + " = " + diff);
+
         }
     }
 }
+/* ١. أكمل على ملف التمرين السابق
+٢. أنشئ متغيرات بداخلها معلومات بطل آخر .2 heroName, heroHeight, heroAge, heroSuperPower1, heroSuperPower2
+٣. أنشئ متغير اسمه. ageDifference و احسب الفرق بين البطل اللذي أنشأته في التمرين السابق و بين البطل هذا
+٤. قم بطباعة فارق العمر بال Console */
+
